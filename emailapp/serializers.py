de52f8a645c74b9ae4515fc74e36586599c9fdaf
Matthias Mulugeta <matthias22m@gmail.com>
2024-03-18 21:59:59 +0300

    removed: to email from seralizers

--- emailapp/serializers.py
+++ emailapp/serializers.py
@@ -4,5 +4,5 @@ from .models import Email
 class EmailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Email
-        fields = ['to_email', 'from_email', 'full_name', 'phone_number', 'subject', 'body']
+        fields = [ 'from_email', 'full_name', 'phone_number', 'subject', 'body']
 
